f72e0c871fbc0f98348c3dedbc85d50011993ede
szjakub <jakubszkodny@gmail.com>
2024-09-06 18:32:35 +0200

    Initial commit

---
+++ lib/simpletuya.h
@@ -0,0 +1,184 @@
+#ifndef SIMPLETUYA_H
+#define SIMPLETUYA_H
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#define DF_MIN_SIZE 7
+#define DU_MIN_SIZE 4
+
+#define HEADER_SIZE 2
+#define DU_COMMANDS_COUNT 4
+
+#define DATA_BUFFER_SIZE 512
+
+
+typedef enum {
+    TYPE_RAW    = 0x00, // N bytes
+    TYPE_BOOL   = 0x01, // 1 byte
+    TYPE_INT    = 0x02, // 4 bytes
+    TYPE_STR    = 0x03, // N bytes
+    TYPE_CHAR   = 0x04, // 1 byte
+    TYPE_BITMAP = 0x05  // 1/2/4 bytes
+} DataType;
+
+typedef enum {
+    // module commands
+    CMD_HEALTHCHECK     = 0x00,
+    CMD_QUERY_PROD_INFO = 0x01,
+    CMD_WORKING_MODE    = 0x02,
+    CMD_NETWORK_STATUS  = 0x03,
+    CMD_SEND_DATA       = 0x06,
+
+    // MCU commands
+    CMD_RESET_MODULE    = 0x04,
+    CMD_QUERY_DATA      = 0x07,
+} Command;
+
+typedef enum {
+    STATE_RESTARTED = 0x00,
+    STATE_RUNNING   = 0x01
+} LastState;
+
+typedef enum {
+    // The SIM card is not connected.
+    STATUS_1 = 0x00,
+
+    // The module searches for cellular networks.
+    STATUS_2 = 0x01,
+
+    // The module is registered with the cellular network
+    // but not connected to the network.
+    STATUS_3 = 0x02,
+
+    // The module is connected to the network and gets an IP address.
+    STATUS_4 = 0x03,
+
+    // The module has been connected to the cloud.
+    STATUS_5 = 0x04,
+
+    // SIM card registration is denied,
+    // which might be because it is not subscribed to a cellular service provider.
+    STATUS_6 = 0x05,
+
+    // The module is ready for pairing.
+    STATUS_7 = 0x06,
+
+    // Unknown status
+    STATUS_8 = 0xFF,
+} NetworkStatus;
+
+typedef struct DataUnit {
+    uint8_t dpid;
+    uint8_t type;
+    uint16_t value_len;
+
+    union {
+        int int_value;
+        // char & bool
+        uint8_t byte_value;
+        // raw & str & bitmap
+        uint8_t *array_value;
+    };
+} DataUnit;
+
+typedef enum {
+    DT_EMPTY = 0,
+    DT_UNIT,
+    DT_RAW,
+} FrameDType;
+
+typedef struct DataFrame {
+    uint16_t header;
+    uint8_t version;
+    uint8_t command;
+    uint16_t data_len;
+    FrameDType data_type;
+
+    union {
+        // TODO: use struct instead of pointers
+        DataUnit *data_unit;
+        uint8_t *raw_data;
+    };
+
+    uint8_t checksum;
+} DataFrame;
+
+typedef struct BytesArray {
+    size_t len;
+    uint8_t bytes[DATA_BUFFER_SIZE];
+} BytesArray;
+
+typedef struct DataUnitDTO {
+    const uint8_t dpid;
+    const uint8_t type;
+    union {
+        int int_value;
+        uint8_t byte_value;
+        // TODO: use struct instead of pointers
+        BytesArray *array_value;
+    };
+} DataUnitDTO;
+
+typedef struct DataFrameDTO {
+    const uint8_t version;
+    const uint8_t command;
+    const FrameDType data_type;
+    union {
+        // TODO: use struct instead of pointers
+        DataUnit *data_unit;
+        BytesArray *raw_data;
+    };
+} DataFrameDTO;
+
+
+void u16_to_bytes(uint8_t *dest, uint16_t value);
+
+uint8_t calculate_bytes_checksum(uint8_t *, size_t);
+
+bool is_frame_valid(DataFrame *);
+
+char *bucket_to_str(BytesArray *);
+
+char *frame_to_str(DataFrame *);
+
+void free_data_frame(DataFrame *);
+
+void free_data_unit(DataUnit *);
+
+void free_bytes_bucket(BytesArray *);
+
+DataFrame *bytes2df(uint8_t *src, size_t len);
+
+DataUnit *bytes2du(uint8_t *src, size_t len);
+
+void du2bytes(uint8_t *dest, const DataUnit *);
+
+void df2bytes(BytesArray *dest, const DataFrame *frame);
+
+void init_data_unit(DataUnit *du, const DataUnitDTO *params);
+
+extern void init_data_frame(DataFrame *frame, const DataFrameDTO *params);
+
+
+#define bytes_to_decimal(T, bytes)                                              \
+  ({                                                                            \
+    T value = 0;                                                                \
+    size_t j = sizeof(T);                                                       \
+    for (size_t i = 0; i < sizeof(T); i++) {                                    \
+      value |= (T)bytes[--j] << (8 * i);                                        \
+    }                                                                           \
+    value;                                                                      \
+  })
+
+
+#define decimal_to_bytes(dest, value)                                           \
+({                                                                              \
+    uint8_t *le_array = (uint8_t *) &value;                                     \
+    int j = 0;                                                                  \
+    for (int i=sizeof(value) - 1; i>=0; i--) {                                  \
+        dest[j++] = le_array[i];                                                \
+    }                                                                           \
+})
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,139 @@
+#include "Arduino.h"
+#include "HardwareSerial.h"
+
+#include "simpletuya.h"
+
+
+BytesArray healthcheck = {.len=7, .bytes={0x55, 0xAA, 0x00, 0x00, 0x00, 0x00, 0xFF}};
+size_t healthcheck_interval = 15000;
+
+LastState lastState;
+
+HardwareSerial *serial = &Serial1;
+
+void send_healthcheck(void);
+void mcu_handler(void);
+
+
+void setup() {
+    serial->begin(9600);
+}
+
+void loop() {
+    send_healthcheck();
+    mcu_handler();
+}
+
+
+void send_healthcheck(void)
+{
+    static unsigned long last_healthcheck;
+    size_t current_time = millis();
+    if (current_time > last_healthcheck + healthcheck_interval)
+    {
+        last_healthcheck = current_time;
+        serial->write(healthcheck.bytes, healthcheck.len);
+    }
+}
+
+void read_serial(BytesArray *buf) {
+    size_t i = 0;
+    while (serial->available()) {
+        buf->bytes[++i] = serial->read();
+    }
+    buf->len = i;
+}
+
+void write_data_frame(DataFrame *frame) {
+    BytesArray buffer;
+    df2bytes(&buffer, frame);
+    serial->write(buffer.bytes, buffer.len);
+}
+
+void mcu_handler() {
+    BytesArray buffer;
+    read_serial(&buffer);
+    if (buffer.len < DF_MIN_SIZE) { return; }
+    DataFrame *frame = bytes2df(buffer.bytes, buffer.len);
+    if (frame == NULL) { return; }
+
+    switch (frame->command) {
+        // <<--- Heartbeat check (0x00)
+        case CMD_HEALTHCHECK: {
+            if (frame->raw_data[0] == 0x00) {
+                lastState = STATE_RESTARTED;
+            } else {
+                lastState = STATE_RUNNING;
+            }
+            // --->> Query product information (0x01)
+            DataFrame respFrame;
+            DataFrameDTO params = {
+                .version=0x03,
+                .command=CMD_QUERY_PROD_INFO,
+                .data_type=DT_EMPTY,
+            };
+            init_data_frame(&respFrame, &params);
+            write_data_frame(&respFrame);
+            break;
+        }
+        // <<--- Query product information (0x01)
+        case CMD_QUERY_PROD_INFO: {
+            /*
+             * there is no need to handle this response,
+             * just send another request from the initialization chain
+             */
+
+            // --->> Query Working mode (0x02)
+            DataFrame respFrame;
+            DataFrameDTO params = {
+                .version=0x03,
+                .command=CMD_WORKING_MODE,
+                .data_type=DT_EMPTY,
+            };
+            init_data_frame(&respFrame, &params);
+            write_data_frame(&respFrame);
+            break;
+        }
+        // <<--- Query working mode (0x02)
+        case CMD_WORKING_MODE: {
+            /*
+             * there is no need to handle this response,
+             * just send another request from the initialization chain
+             */
+
+            // --->> Report network status (0x03)
+            DataFrame respFrame;
+            BytesArray networkStatusData = {.len=1, .bytes={STATUS_4}};
+            DataFrameDTO params = {
+                .version=0x03,
+                .command=CMD_NETWORK_STATUS,
+                .data_type=DT_RAW,
+                .raw_data=&networkStatusData
+            };
+            init_data_frame(&respFrame, &params);
+            write_data_frame(&respFrame);
+            break;
+        }
+            // <<--- Report network status (0x03)
+        case CMD_NETWORK_STATUS: {
+            /*
+             * there is no need to handle this response,
+             * just send another request from the initialization chain
+             */
+
+            // --->> Heartbeat check after the initialization process
+            DataFrame respFrame;
+            DataFrameDTO params = {
+                .version=0x03,
+                .command=CMD_HEALTHCHECK,
+                .data_type=DT_EMPTY,
+            };
+            init_data_frame(&respFrame, &params);
+            write_data_frame(&respFrame);
+            break;
+        }
+    }
+    free_data_frame(frame);
+}
+
+
