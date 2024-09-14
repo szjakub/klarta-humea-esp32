4ea8642f1a48b2e09548d685c12dfe0d7f4cd2a4
szjakub <jakubszkodny@gmail.com>
2024-09-14 10:32:31 +0200

    Debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ extern "C"
 #include "simpletuya.h"
 }
 
+
 #define HEALTHCHECK_INTERVAL 15000 // ms
 
 HardwareSerial *serial = &Serial1;
@@ -15,6 +16,9 @@ void mcu_handler(void);
 void write_data_frame(DataFrame *frame);
 
 void setup() {
+#ifdef DEBUG
+    Serial.begin(115200);
+#endif
     serial->begin(9600);
 }
 
@@ -25,22 +29,42 @@ void loop() {
 
 void read_serial(BytesArray *buf) {
     size_t i = 0;
+    if (serial->available() > 0){
+        delayMicroseconds(800);
+    }
     while (serial->available()) {
-        buf->bytes[++i] = serial->read();
+        uint8_t in_byte = serial->read();
+        buf->bytes[++i] = in_byte;
+        delayMicroseconds(800);
     }
     buf->len = i;
+
+#ifdef DEBUG
+    if (buf->len > 1) {
+        char *str = bytes_array_to_str(buf);
+        Serial.print("RX ");
+        Serial.println(str);
+        free(str);
+    }
+#endif
 }
 
 void write_data_frame(DataFrame *frame) {
     BytesArray buffer;
     df2bytes(&buffer, frame);
+#ifdef DEBUG
+    char *str = bytes_array_to_str(&buffer);
+    Serial.print("TX ");
+    Serial.println(str);
+    free(str);
+#endif
     serial->write(buffer.bytes, buffer.len);
 }
 
 void send_healthcheck(void)
 {
-    static size_t last_healthcheck;
-    size_t current_time = millis();
+    static unsigned long last_healthcheck = 0;
+    unsigned long current_time = millis();
     if (current_time > last_healthcheck + HEALTHCHECK_INTERVAL)
     {
         DataFrame respFrame;
@@ -55,13 +79,23 @@ void send_healthcheck(void)
     }
 }
 
+
 void mcu_handler() {
     static size_t lastState;
     BytesArray buffer;
+    memset(&buffer, 0, sizeof(BytesArray));
     read_serial(&buffer);
     if (buffer.len < DF_MIN_SIZE) { return; }
     DataFrame *frame = bytes2df(buffer.bytes, buffer.len);
     if (frame == NULL) { return; }
+#ifdef DEBUG
+    if (is_frame_valid(frame)) {
+        Serial.println("VALID");
+    } else {
+        Serial.println("INVALID");
+    }
+#endif
+
 
     switch (frame->command) {
         // <<--- Heartbeat check (0x00)
